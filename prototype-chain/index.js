4e8602b1e2d20fbcb8f42b6304a6ddb4999a1b30
dongwt <834575475@qq.com>
2016-05-12 13:44:21 +0800

    prototype chain

--- prototype-chain/index.js
+++ prototype-chain/index.js
@@ -15,3 +15,24 @@ var person = new Person();
 console.log("Object.prototype:" + typeof Object.prototype);
 console.log("Function.prototype:" + typeof Function.prototype);
 
+
+function printPrototypeChain(obj){
+    var index = 1;
+    while((obj = obj.__proto__)){
+        console.log(index + ":");
+        console.log(obj);
+        index++;
+    }
+}
+
+console.log("---------------new String(dongwt)-------------------");
+printPrototypeChain(new String("dongwt"));
+console.log("---------------new Array()-------------------");
+printPrototypeChain(new Array());
+console.log("---------------new Object()-------------------");
+printPrototypeChain(new Object());
+console.log("---------------Function-------------------");
+printPrototypeChain(Function);
+console.log("---------------Object-------------------");
+printPrototypeChain(Object);
+
