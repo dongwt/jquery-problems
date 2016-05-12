710683f1d7def79b9986136f061348944a035581
dongwt <834575475@qq.com>
2016-05-12 14:38:26 +0800

    typeof vs instanceof

--- method/typeof-instanceof/index.js
+++ method/typeof-instanceof/index.js
@@ -8,6 +8,13 @@
 //你不知道它到底是一个object对象，还是数组，还是new Number等等。
 //这个时候就需要用到instanceof
 
+
+//Instanceof运算符的第一个变量是一个对象，暂时称为A；第二个变量一般是一个函数，暂时称为B。
+//Instanceof的判断队则是：沿着A的__proto__这条线来找，同时沿着B的prototype这条线来找，
+//如果两条线能找到同一个引用，即同一个对象，那么就返回true。如果找到终点还未重合，则返回false。
+
+//instanceof表示的就是一种继承关系，或者原型链的结构
+
 console.log("typeof");
 console.log("NULL:" + typeof null);
 console.log("Array:" + typeof new Array());
@@ -21,6 +28,11 @@ console.log("0:" + typeof(0));
 console.log("instanceof");
 console.log("new Array() instanceof Array:" + (new Array() instanceof Array));
 console.log("new Array() instanceof Array:" + (new Array() instanceof Object));
+
+
+console.log("Object instanceof Function:" + (Object instanceof Function));
+console.log("Function instanceof Object:" + (Function instanceof Object));
+console.log("Function instanceof Function:" + (Function instanceof Function));
 
 
 
