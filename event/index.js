65bae747ef16b273bc8080c3de72f1564794faed
dongwt <834575475@qq.com>
2016-06-16 10:51:01 +0800

    event

--- event/index.js
+++ event/index.js
@@ -1,15 +1,24 @@
-$(document).on("mouseover","#btn1",function(event){
-    var $this = $(event.currentTarget);
-    var e = $.Event("btn1.hover");
-    $this.trigger(e);
+$(document).on("mouseover", "#btn1", function (event) {
+  var $this = $(event.currentTarget);
+  var e = $.Event("btn1.hover");
+  $this.trigger(e);
 
-    if(e.isDefaultPrevented()){
-        console.log("btn1.hover  isDefaultPrevented");
-    }
+  if (e.isDefaultPrevented()) {
+    console.log("btn1.hover  isDefaultPrevented");
+  }
 });
 
-$("#btn1").on("btn1.hover",function(event){
-    event.preventDefault();
-    console.log("btn1.hover");
+$("#btn1").on("btn1.hover", function (event) {
+  event.preventDefault();
+  console.log("btn1.hover");
+});
+
+
+$(document).on("click.test", "#btn3", function () {
+  console.log("click.test");
+});
+
+$(document).on("keydown", "#text1", function (e) {
+  console.log(e.type + ': ' + e.which);
 });
 
